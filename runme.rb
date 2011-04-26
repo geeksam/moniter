3d55e7689cf61369bf5593f5919c78bf14f53ede
Sam Livingston-Gray <sam@renewfund.com>
2011-04-26 10:24:48 -0700

    Tweak schedule and add some helpful reminders

--- runme.rb
+++ runme.rb
@@ -2,12 +2,22 @@
 require File.join(File.dirname(__FILE__), *%w[lib moniter])
 
 Moniter do
+  clock_resolution 5.seconds
+
   to_notify_via :growl do |message|
     `growlnotify -s -t "Iteration Timer" -m "#{message}"`
   end
 
   to_notify_via :speech do |message|
     `say "#{message}"`
+  end
+
+  to_notify_via :whats_next do
+    `growlnotify -s -t "What's Next?" -m "Wrap up, and consider what you'll do next iteration..."`
+  end
+
+  to_notify_via :harvest_reminder do
+    `growlnotify -s -t "Time Tracking" -m "Time to update Harvest!"`
   end
 
   iteration :starts_at => '09:00 AM', :ends_at => '10:30 AM'
@@ -18,10 +28,13 @@ Moniter do
 
   each_iteration do
     notify_at :start, :via => [:growl, :speech]
-    notify_when 30.minutes => :elapsed, :via => [:growl]
-    notify_when 30.minutes => :remain,  :via => [:growl]
-    notify_when 15.minutes => :remain,  :via => [:growl, :speech]
-    notify_when  5.minutes => :remain,  :via => [:growl, :speech]
-    notify_at :end, :via => [:growl, :speech]
+
+    notify_when 60.minutes => :remain, :via => [:growl]
+    notify_when 30.minutes => :remain, :via => [:growl]
+    notify_when 15.minutes => :remain, :via => [:growl]
+    notify_when 10.minutes => :remain, :via => [:growl, :whats_next]
+    notify_when  5.minutes => :remain, :via => [:growl, :speech, :harvest_reminder]
+
+    notify_at :end, :via => [:growl, :speech, :harvest_reminder]
   end
 end
